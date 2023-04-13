4b5de1bf18df8e99e82ced7bff348a7cc3f683a2
Jonathan Ruge <53587500+ruge2109-Git@users.noreply.github.com>
2023-04-12 21:25:55 -0500

    feat: add sectionWrapper and about section

--- src/constants/index.js
+++ src/constants/index.js
@@ -40,6 +40,10 @@ export const navLinks = [
   },
 ];
 
+const basicInformation = {
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+}
+
 const services = [
   {
     title: "Web Developer",
@@ -262,4 +266,4 @@ const projects = [
   },
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+export { basicInformation, services, technologies, experiences, testimonials, projects };
